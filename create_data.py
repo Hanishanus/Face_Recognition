27edabfbc66ed84089622881e511430a35b343c2
Hanishanus <113075597+Hanishanus@users.noreply.github.com>
2025-05-05 09:49:59 +0530

    Dataset creation successfully completed

--- create_data.py
+++ create_data.py
@@ -22,3 +22,19 @@ if not os.path.isdir(path):
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+            face = gray[y:y+h, x:x+w]
+            face_resize = cv2.resize(face, (width,height))# to resize the images with same height and width
+            cv2.imwrite('%s/%s.png' % (path,count), face_resize)#to take images upto 50 and store as png files
+            count += 1
+
+            cv2.imshow('OpenCV', im)
+            key = cv2.waitKey(10)
+            if key == 27:
+                break
+
+
+    webcam.release()
+    cv2.destroyAllWindows()
